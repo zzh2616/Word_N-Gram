7a4aa949b0a4043d541286a5d648625e59f23249
zzh2616 <zzh2616@hotmail.com>
2018-03-11 19:28:31 +0100

    markov word two

--- MarkovWordTwo.java
+++ MarkovWordTwo.java
@@ -8,11 +8,11 @@
 
 import java.util.*;
 
-public class MarkovWordOne implements IMarkovModel {
+public class MarkovWordTwo implements IMarkovModel {
     private String[] myText;
     private Random myRandom;
 
-    public MarkovWordOne() {
+    public MarkovWordTwo() {
         myRandom = new Random();
     }
 
@@ -26,10 +26,12 @@ public class MarkovWordOne implements IMarkovModel {
 
 	public String getRandomText(int numWords){
 		StringBuilder sb = new StringBuilder();
-		int index = myRandom.nextInt(myText.length-1);  // random word to start with
-		String key = myText[index];
-		sb.append(key);
-		sb.append(" ");
+		int index = myRandom.nextInt(myText.length-2);  // random word to start with
+		String key[] = {myText[index], myText[index+1]};
+    for (int i = 0; i< key.length; i++) {
+      sb.append(key[i]);
+  		sb.append(" ");
+    }
 		for(int k=0; k < numWords-1; k++){
 		    ArrayList<String> follows = getFollows(key);
 		    if (follows.size() == 0) {
@@ -39,17 +41,26 @@ public class MarkovWordOne implements IMarkovModel {
 			String next = follows.get(index);
 			sb.append(next);
 			sb.append(" ");
-			key = next;
+      for (int i = 0; i< key.length-1; i++) {
+        key[i] = key[i+1];
+      }
+      key[key.length-1] = next;
 		}
 
 		return sb.toString().trim();
 	}
 
   // method that finds the position of a certain word "target" in a string array "words"; if no word found, method returns -1
-  public int indexOf(String[] words, String target, int start){
+  public int indexOf(String[] words, String[] target, int start){
     int ans = -1;
-    for (int i = start; i < words.length; i++) {
-      if (words[i].equals(target)) {
+    for (int i = start; i < words.length - target.length - 1; i++) {
+      boolean flag = true;
+      for (int k = 0; k < target.length ; k++) {
+        if (!words[i+k].equals(target[k])) {
+          flag = false;
+        }
+      }
+      if (flag) {
         ans = i;
         break;
       }
@@ -57,13 +68,13 @@ public class MarkovWordOne implements IMarkovModel {
     return ans;
   }
 
-	private ArrayList<String> getFollows(String key) {
+	private ArrayList<String> getFollows(String[] key) {
 	    ArrayList<String> follows = new ArrayList<String>();
       int idx = 0;
       while (indexOf(myText, key, idx) != -1) {
         idx = indexOf(myText, key, idx);
-        if(idx >= myText.length - 1) break;
-        follows.add(myText[idx+1]);
+        if(idx >= myText.length - 2) break;
+        follows.add(myText[idx+2]);
         idx++;
       }
 	    return follows;
